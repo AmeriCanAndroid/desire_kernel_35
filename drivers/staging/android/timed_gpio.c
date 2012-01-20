eb94fc9b5dff3284c56325f09dd4100cb08dfc26
warrenb213 <AmeriCanAndroidROM@gmail.com>
2012-01-19 21:50:13 -0800

    Stop kernel from spamming dmesg log while it vibrates (marc1706)

--- drivers/staging/android/timed_gpio.c
+++ drivers/staging/android/timed_gpio.c
@@ -76,8 +76,11 @@ static void gpio_enable(struct timed_output_dev *dev, int value)
 	unsigned long	flags;
 	int rc;
 
+	/*
+	* stop spamming dmeg -- marc1706
 	printk(KERN_INFO "%s(parent:%s): vibrates %d msec\n",
 		current->comm, current->parent->comm, value);
+	*/
 	spin_lock_irqsave(&data->lock, flags);
 
 	/* cancel previous timer and set GPIO according to value */
